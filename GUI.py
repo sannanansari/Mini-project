eb1334877f4dea039abff68b08ad9e6980d165b0
Sanan <30230441+sannanansari@users.noreply.github.com>
2019-03-04 11:08:31 +0530

    Update GUI.py
    Input Entry in GUI

--- GUI.py
+++ GUI.py
@@ -10,28 +10,28 @@ gui = Tk()
 gui.state('zoomed')
 gui.configure(background='#38A1F3')
 
-#Label
+#Label-heading
 l = Label(gui,text='Welcome to Twitter sentimental Analysis',fg='white',bg= '#38A1F3',height=10)
 font = ('times',20,'bold')
 l.config(font=font)
 l.pack(anchor='center')
 
-#Label
-
+#Label-text
 label = Label(gui,text='Enter the input Here',bg='#38A1F3',fg='white').pack()
 label = Label(gui,text='Enter the input Here',fg='#38A1F3',bg='#38A1F3').pack()
 
 #Input
-entry = Entry(gui,bd=3,width=45)
-entry.pack()
+v = StringVar()
+entry = Entry(gui,bd=3,width=45,textvariable=v).pack()
 label = Label(gui,text='Enter the input Here',fg='#38A1F3',bg='#38A1F3').pack()
 
 
 #button
 def newframe():
     frame = Toplevel(gui)
-    label = Label(frame,text='New window').pack()
+    label = Label(frame,text=v.get()).pack()
     button = Button(frame,text='click here to quit',command=frame.destroy).pack()
+
 button = Button(gui,text='Calculate sentiment',command=newframe).pack()
 
 gui.mainloop()
